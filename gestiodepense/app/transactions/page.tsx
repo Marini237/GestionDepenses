38fe31e248b3e0bffecd3ec835cda0247f11a873
Axel23 <kanamarini@gmail.com>
2024-11-11 20:50:30 +0100

    nouveau projet

---
+++ gestiodepense/app/transactions/page.tsx
@@ -0,0 +1,74 @@
+"use client";
+import { Transaction } from "@/type";
+import { useUser } from "@clerk/nextjs";
+import React, { useEffect, useState } from "react";
+import { getTransactionsByEmailAndPeriod } from "../actions";
+import Wrapper from "../components/Wrapper";
+import TransactionItem from "../components/TransactionItem";
+
+const Page = () => {
+  const { user } = useUser();
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const fetchTransactions = async (period: string) => {
+    if (user?.primaryEmailAddress?.emailAddress) {
+      setLoading(true);
+      try {
+        const transactionsData = await getTransactionsByEmailAndPeriod(
+          user?.primaryEmailAddress?.emailAddress,
+          period
+        );
+        setTransactions(transactionsData);
+        setLoading(false);
+      } catch (err) {
+        console.error("Erreur lors de la récupération des transactions:", err);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchTransactions("last30");
+  }, [user?.primaryEmailAddress?.emailAddress]);
+  return (
+    <div>
+      <Wrapper>
+        <div className="flex justify-end mb-5">
+          <select
+            className="input input-bordered input-md"
+            defaultValue="last30"
+            onChange={(e) => fetchTransactions(e.target.value)}
+          >
+            <option value="last7">7 derniers jours</option>
+            <option value="last30">30 derniers jours</option>
+            <option value="last90">90 Derniers jours</option>
+            <option value="last365">365 derniers jours</option>
+          </select>
+        </div>
+        <div className="overflow-x-auto x-full bg-base-200/35 p-5 rounded-xl">
+          {loading ? (
+            <div className="flex justify-center items-center">
+              <span className="loading loading-spinner loading-md"></span>
+            </div>
+          ) : transactions.length === 0 ? (
+            <div className="flex justify-center items-center h-full">
+              <span className="text-gray-500 text-sm">
+                Aucune transaction trouvée
+              </span>
+            </div>
+          ) : (
+            <ul className="divide-y devide-base-300">
+              {transactions.map((transaction) => (
+                <TransactionItem
+                  key={transaction.id}
+                  transaction={transaction}
+                />
+              ))}
+            </ul>
+          )}
+        </div>
+      </Wrapper>
+    </div>
+  );
+};
+
+export default Page;
