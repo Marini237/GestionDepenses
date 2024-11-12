daa55bf1c0c67cda3348f352f8b737f9b7ed9049
Axel23 <kanamarini@gmail.com>
2024-11-12 13:08:49 +0100

    modification de nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,15 @@
+// next.config.js
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // Active le mode strict de React pour détecter les erreurs potentiels
-  swcMinify: true, // Active la minification avec le compilateur SWC pour des builds plus rapides
-
-  // Désactiver ESLint pendant le build en production (option temporaire)
+  reactStrictMode: true,
+  swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-
-  // Configuration pour les exports statiques (si vous utilisez `next export`)
-  output: 'export',
-
-  // Configurer des variables d’environnement public (accessibles côté client)
   env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL, // exemple
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
-
-  // Ajoutez d'autres configurations si nécessaire
 };
 
 export default nextConfig;
